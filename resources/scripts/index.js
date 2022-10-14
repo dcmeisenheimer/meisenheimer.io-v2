0aa37e4fa063b1270bfbde020948a88b62923bc5
Daniel <70774313+dcmeisenheimer@users.noreply.github.com>
2022-10-13 21:52:30 -0500

    Added About and Project Skeleton

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,8 +1,55 @@
+//Menu Home Effect
 const menu = document.getElementById("menu");
 
 Array.from(document.getElementsByClassName("menu-item"))
     .forEach((item, index) => {
         item.onmouseover = () => {
             menu.dataset.activeIndex = index;
-        }
-    });
+    }
+});
+
+//Shape Animation
+const wrapper = document.getElementById("wrapper");
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const uniqueRand = (min, max, prev) => {
+  let next = prev;
+  
+  while(prev === next) next = rand(min, max);
+  
+  return next;
+}
+
+const combinations = [
+  { configuration: 1, roundness: 1 },
+  { configuration: 1, roundness: 2 },
+  { configuration: 1, roundness: 4 },
+  { configuration: 2, roundness: 2 },
+  { configuration: 2, roundness: 3 },
+  { configuration: 3, roundness: 3 }
+];
+
+let prev = 0;
+
+setInterval(() => {
+  const index = uniqueRand(0, combinations.length - 1, prev),
+        combination = combinations[index];
+  
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
+  
+  prev = index;
+}, 3000);
+
+//Card Hover Effect
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
